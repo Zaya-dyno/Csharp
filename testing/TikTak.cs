158a0abc3c6796ab0578b3db735a0230a8eb6e28
tuvshinzaya erdenekhuu <tuvshinzaya.erdenekhuu@arm.com>
2023-10-03 10:47:34 +0100

    Fixed inconsistent usage of tab and spaces

--- testing/TikTak.cs
+++ testing/TikTak.cs
@@ -2,26 +2,29 @@
 using System.Text.RegularExpressions;
 namespace testing
 {
-	public class TikTak
-	{
-		short[,] board;
+    public class TikTak
+    {
+        short[,] board;
         static int gameNumber;
         Regex allowMove = new Regex("^[1-3]{2}$");
 
-        static void clearBoard(short[,] board){
-            for (int i=0; i<3; i++){
-                for (int j=0; j<3; j++){
-                    board[i,j] = 0;
+        static void clearBoard(short[,] board)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    board[i, j] = 0;
                 }
             }
         }
 
-		public TikTak()
-		{
-			board = new short[3, 3];
+        public TikTak()
+        {
+            board = new short[3, 3];
             clearBoard(board);
             gameNumber = 0;
-		}
+        }
 
         public void startGame()
         {
@@ -35,19 +38,21 @@ namespace testing
                 winner = gameEnd();
                 Player1 = !Player1;
             }
-            if ( winner == 3)
+            if (winner == 3)
             {
                 Console.WriteLine("Hopefully next time you guys can figure out who is better");
-            } else if (winner == 1)
+            }
+            else if (winner == 1)
             {
                 Console.WriteLine("Congrats Player 1 you won");
-            } else
+            }
+            else
             {
                 Console.WriteLine("Congrats Player 2 you won");
             }
 
         }
-        
+
         void takeTurn(bool Player1)
         {
             string name;
@@ -61,15 +66,15 @@ namespace testing
             }
             while (true)
             {
-            Console.Write($"{name} make your turn\nEnter your move:");
-            string move = Console.ReadLine();
-            if (allowMove.IsMatch(move))
-            {
-                if (makeMove(Player1,move))
+                Console.Write($"{name} make your turn\nEnter your move:");
+                string move = Console.ReadLine();
+                if (allowMove.IsMatch(move))
                 {
-                    break;
+                    if (makeMove(Player1, move))
+                    {
+                        break;
+                    }
                 }
-            }
             }
             printScreen();
         }
@@ -86,7 +91,7 @@ namespace testing
             return true;
         }
 
-        void recordMove(bool Player1, (int,int) m)
+        void recordMove(bool Player1, (int, int) m)
         {
             short draw = 2;
             if (Player1)
@@ -99,17 +104,19 @@ namespace testing
         short gameEnd()
         {
             // 0 - game going, 1 - player1 win,  2 - player2 win, 3 - draw
-            (int,int) b = encodeBoard();
+            (int, int) b = encodeBoard();
             Console.WriteLine(b);
             bool win1 = checkWin(b.Item1);
             bool win2 = checkWin(b.Item2);
-            if ( (b.Item1 | b.Item2) == 0x1FF )
+            if ((b.Item1 | b.Item2) == 0x1FF)
             {
                 return 3;
-            } else if (win1)
+            }
+            else if (win1)
             {
                 return 1;
-            } else if (win2)
+            }
+            else if (win2)
             {
                 return 2;
             }
@@ -118,53 +125,57 @@ namespace testing
 
         bool checkWin(int pos)
         {
-            for (int i=0; i<3; i++)
+            for (int i = 0; i < 3; i++)
             {
-                if ( (pos & (7 << (i * 3)) ) == (7 << (i * 3)))
+                if ((pos & (7 << (i * 3))) == (7 << (i * 3)))
                 {
                     return true;
-                } else if ( (pos & (0b1_001_001 << i)) == (0b1_001_001 << i))
+                }
+                else if ((pos & (0b1_001_001 << i)) == (0b1_001_001 << i))
                 {
                     return true;
                 }
             }
-            if ( (pos & 0b100_010_001) == 0b100_010_001)
+            if ((pos & 0b100_010_001) == 0b100_010_001)
             {
                 return true;
-            } else if ( (pos & 0b001_010_100) == 0b001_010_100)
+            }
+            else if ((pos & 0b001_010_100) == 0b001_010_100)
             {
                 return true;
             }
             return false;
         }
 
-        (int,int) encodeBoard()
+        (int, int) encodeBoard()
         {
-            (int, int) ret = (0,0);
+            (int, int) ret = (0, 0);
             for (int i = 0; i < 3; i++)
             {
                 for (int j = 0; j < 3; j++)
                 {
-                    if (board[i, j] == 1) ret.Item1 |= 1 << (i*3 + j);
+                    if (board[i, j] == 1) ret.Item1 |= 1 << (i * 3 + j);
                     else if (board[i, j] == 2) ret.Item2 |= 1 << (i * 3 + j);
                 }
             }
             return ret;
         }
 
-		void printScreen()
-		{
-            for(int i=0; i<3; i++){
-                for(int j=0; j<3; j++){
+        void printScreen()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
                     char p = ' ';
-                    if (board[i,j] == 1) p = '+';
-                    else if (board[i,j] == 2) p = '-';
+                    if (board[i, j] == 1) p = '+';
+                    else if (board[i, j] == 2) p = '-';
                     Console.Write(p);
-                    if(j != 2)
+                    if (j != 2)
                     {
                         Console.Write('|');
                     }
-                        
+
                 }
                 Console.WriteLine();
                 if (i != 2)
@@ -172,18 +183,19 @@ namespace testing
                     Console.WriteLine("-----");
                 }
             }
-		}
+        }
     }
 
     class MainClass
     {
-        static void Main(string[] args){
+        static void Main(string[] args)
+        {
             TikTak game = new TikTak();
             while (true)
             {
                 game.startGame();
                 string repeat = "";
-                while(repeat == "")
+                while (repeat == "")
                 {
                     repeat = Console.ReadLine();
                     if (repeat == "again" || repeat == "quit")
@@ -192,7 +204,7 @@ namespace testing
                     }
                     repeat = "";
                 }
-                if (repeat == "quit" )
+                if (repeat == "quit")
                 {
                     Console.WriteLine("Thank for enjoying the game");
                     break;
@@ -202,4 +214,3 @@ namespace testing
     }
 
 }
-
